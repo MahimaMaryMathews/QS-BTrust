b8a643003213e2d9986e333cb72cd91a15ad2b84
unknown <mahimamarymathews@gmail.com>
2026-04-24 03:07:32 +0530

    review_changes

--- core/puf.py
+++ core/puf.py
@@ -1,12 +1,25 @@
 # core/puf.py
 import hashlib
+import numpy as np
+from pypuf.simulation import XORArbiterPUF
 
 class PUF:
     def __init__(self, secret):
         self.secret = secret
 
+        self.n = 64
+        self.puf = XORArbiterPUF(n=self.n, k=4)
+
     def evaluate(self, challenge, ts_req, ts_res):
-        raw = self.secret + challenge
+        challenge_bits = np.unpackbits(
+            np.frombuffer(challenge, dtype=np.uint8)
+        )[:self.n]
+
+        challenge_bits = 2 * challenge_bits - 1
+
+        response = self.puf.eval(challenge_bits)
+
+        raw = self.secret + str(response).encode()
         puf_out = hashlib.sha256(raw).digest()
 
         return hashlib.sha256(
